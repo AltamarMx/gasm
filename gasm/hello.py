f16f65b628de2c81ebda647aedbbb8a1fd68b124
altamarmx <gbv@ier.unam.mx>
2023-03-30 10:15:40 -0600

    correcciones

---
+++ gasm/hello.py
@@ -0,0 +1,20 @@
+
+def hello_kitty():
+    """
+    Display a random phrase from a list of phrases.
+
+    :param phrases: List of phrases to choose from
+    :type phrases: list
+    """
+    phrases = [
+    "Carpe diem.",
+    "Hakuna matata.",
+    "Keep calm and carry on.",
+    "Seize the day.",
+    "The sky is the limit.",
+    ]
+
+    phrase = random.choice(phrases)
+    print(phrase)
+
+]
